a0b066b45779cc985de7f1b7c3aff3a52e4f4493
ivan0124 <ivan0124.tw@gmail.com>
2016-12-14 10:13:59 +0800

    update code

--- deploy_api_gw.sh
+++ deploy_api_gw.sh
@@ -3,7 +3,7 @@ MQTT_IMAGE=advigw4x86/mqtt-bus
 MQTT_CONTAINER=mqtt
 DOCKER_API_GW_IMAGE=ivan0124tw/docker_api_gw
 DOCKER_API_GW_CONTAINER=docker_api_gw
-ADVANTECH_NET=advantech-net
+ADVANTECH_NET=advigw_network
 
 
 #stop container
@@ -37,15 +37,19 @@ fi
 NET=`sudo docker network ls | grep $ADVANTECH_NET | awk '{ print $2}'`
 if [ "$NET" != "$ADVANTECH_NET" ] ; then
 echo "======================================="
-echo "$ADVANTECH_NET does not exist, create $ADVANTECH_NET network..."
+echo "[Step4]: $ADVANTECH_NET does not exist, create $ADVANTECH_NET network..."
 echo "======================================="
-sudo docker network create -d bridge $ADVANTECH_NET
+sudo docker network create -d bridge --subnet 172.25.0.0/16 i$ADVANTECH_NET
+else
+echo "======================================="
+echo "[Step4]: Found $ADVANTECH_NET network. $ADVANTECH_NET exist."
+echo "======================================="
 fi
 
 #run container and join to `advantech-net` network
 echo "======================================="
 echo "[Step4]: Run container images......"
 echo "======================================="
-sudo docker run --network=$ADVANTECH_NET -itd --name $MQTT_CONTAINER $MQTT_IMAGE
+sudo docker run --network=$ADVANTECH_NET -itd --name $MQTT_CONTAINER -p 1883:1883 $MQTT_IMAGE
 #sudo docker run --network=$ADVANTECH_NET -it --name $DOCKER_API_GW_CONTAINER $DOCKER_API_GW_IMAGE
 sudo docker run --network=$ADVANTECH_NET -it --name $DOCKER_API_GW_CONTAINER -v $PWD/APIGateway:/home/adv/APIGateway:rw -v /usr/share/webmin/wsn_setting:/home/adv/wsn_setting:rw -p 3000:3000 $DOCKER_API_GW_IMAGE
